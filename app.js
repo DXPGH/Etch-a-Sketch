b75f80b1706c0363daccee30f91d13b6e841cbca
DXPGH <dxpbusiness@gmail.com>
2022-07-27 19:44:36 -0500

    Etch a Sketch complete

---
+++ app.js
@@ -0,0 +1,60 @@
+let color = "black";
+let click = true;
+
+function populateGrid(size) {
+  let gridContainer = document.querySelector(".grid-container");
+  let squares = gridContainer.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  gridContainer.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+  let amount = size * size;
+
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    gridContainer.insertAdjacentElement("beforeend", square);
+  }
+}
+
+populateGrid(16);
+
+function changeSize(input) {
+  if (input >= 2 || input <= 100) {
+    document.querySelector('.error').style.display = 'none';
+    populateGrid(input);
+  } else {
+    document.querySelector('.error').style.display = 'flex';
+  }
+}
+
+function colorSquare() {
+  if (click) {
+    if (color === "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetGrid() {
+  let gridContainer = document.querySelector(".grid-container");
+  let squares = gridContainer.querySelectorAll("div");
+  squares.forEach((square) => (square.style.backgroundColor = "white"));
+}
+
+document.querySelector("body").addEventListener("click", (e) => {
+  if (e.target.tagName != "BUTTON") {
+    click = !click;
+    if (click) {
+      document.querySelector(".mode").textContent = "Mode: Coloring";
+    } else {
+      document.querySelector(".mode").textContent = "Mode: Not Coloring";
+    }
+  }
+});
